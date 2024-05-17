3ba4f10ea20b0be86da667725a69caac9a1d7a61
Brightingbright <bagbemenu@st.ug.edu.gh>
2024-05-17 20:04:04 +0000

    completed task 2

--- arrayManipulations.js
+++ arrayManipulations.js
@@ -11,3 +11,37 @@ function processArray(numbers) {
 const inputArray = [1, 2, 3, 4, 5];
 const resultArray = processArray(inputArray);
 console.log(resultArray); 
+
+
+function processArray(numbers) {
+    return numbers.map(number => {
+        if (number % 2 === 0) {
+            return number * number; 
+        } else {
+            return number * 3; 
+        }
+    });
+}
+
+
+function formatArrayStrings(strings, numbers) {
+    if (strings.length !== numbers.length) {
+        throw new Error("The lengths of the two arrays must be the same.");
+    }
+
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase(); 
+        } else {
+            return str.toLowerCase(); 
+        }
+    });
+}
+
+
+const strings = ["jesus", "is", "Coming", "soon"];
+const numbers = [1, 2, 3, 4];
+const processedNumbers = processArray(numbers);
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+
+console.log(formattedStrings); 
